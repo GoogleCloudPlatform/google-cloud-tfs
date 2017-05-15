06bd522d533c45bc7a97b22f2ad94c1994c6c1f9
Jim Przybylinski <przybjw@google.com>
2017-05-15 16:07:32 -0700

    Enable AppVeyor (#7)
    * Modify MSBuild files to enable npm install the common modules as part of their build.
    
    * Add BuildFunctions.psm1 and change BuildExtension.ps1 to use it.
    
    * Add appveyor.yml config file to enable AppVeyor.
    
    * Add Build status to README.md.

--- build/BuildExtension.ps1
+++ build/BuildExtension.ps1
@@ -13,166 +13,51 @@
  # See the License for the specific language governing permissions and
  # limitations under the License.
  ##>
-
 [CmdletBinding()]
-Param([string[]] $TasksToBuild, [switch]$VerboseTest, [switch]$SkipTests, [switch]$SkipNpm, [switch]$RevVersion)
+Param([string[]]$TasksToBuild, [switch]$SkipInit, [switch]$SkipCompile, [switch]$SkipTest)
 
-$allTasks = (
-    "deploy-gke-build-task",
-    "container-build-task",
-    "deploy-gae-build-task",
-    "set-login-build-task"
-)
 
-if($TasksToBuild -eq $null) {
-    $tasks = $allTasks
-} else {
-    $tasks = $allTasks | ? {$_ -in $TasksToBuild}
-    $TasksToBuild |
-        ? { $_ -notin $allTasks } |
-        % { Write-Warning "$_ is not a valid task" }
-}
+$functionsModule = Import-Module ./BuildFunctions.psm1 -PassThru
+pushd (Join-Path $MyInvocation.MyCommand.Path ..)
+try {
+    cd ..
 
-function Run($buildScriptPath)
-{
-    pushd (Join-Path $buildScriptPath ..)
-    try {
-        cd ..
+    $allTasks = GetTypeScriptTaskModules
 
-        if(Test-Path bin) {
-            Write-Verbose "Removing bin"
-            rm bin -Force -Recurse -ErrorAction Stop
-        }
-
-        BuildCommon
-        InitPsTask "install-cloud-sdk-build-task"
-        $tasks | % {
-            BuildTask $_
-        }
-
-        Write-Host "Building package"
-        if($RevVersion) {
-            Write-Verbose "Running: tfx extension create --manifestGlobs **/manifest.json --output-path bin --rev-version"
-            tfx extension create --manifestGlobs **/manifest.json --output-path bin --rev-version
-        } else {
-            Write-Verbose "Running: tfx extension create --manifestGlobs **/manifest.json --output-path bin"
-            tfx extension create --manifestGlobs **/manifest.json --output-path bin
-        }
-        Write-Host "End build package"
-    } finally {
-        popd
+    if($TasksToBuild -eq $null) {
+        $tasks = $allTasks
+    } else {
+        $tasks = $allTasks | ? {$_ -in $TasksToBuild}
+        $TasksToBuild |
+            ? { $_ -notin $allTasks } |
+            % { Write-Warning "$_ is not a valid task" }
     }
-}
 
-function BuildTask($task) {
-    Write-Host "Building task $task"
-    pushd $task
-    try {
-        if (!$SkipNpm) {
-            Write-Verbose "Running: npm install"
-            npm install
-        }
-        # Common module changes as part of development. Update the common module every time.
-        npm install ../common | Out-Null
-
-        Write-Verbose "Running: tsc"
-        tsc
-
-        if ($LASTEXITCODE -ne 0) {
-            throw "tsc failed for task $task"
-        }
-
-        $testSuite = Join-Path Test _suite.js
-
-        if (!$SkipTests -and (Test-path $testSuite)) {
-            $oldEnvVar = $env:TASK_TEST_TRACE
-            if ($VerboseTest) {
-                $env:TASK_TEST_TRACE = $true
-            }
-            Write-Verbose "Running: mocha $testSuite"
-            mocha $testSuite
-            $env:TASK_TEST_TRACE = $oldEnvVar
-
-            if ($LASTEXITCODE -ne 0) {
-                throw "mocha failed  for task $task"
-            }
-        } else {
-            Write-Warning "Skipping tests for task $task"
-        }
-
-        Write-Verbose "Copying icon.png"
-        cp ..\images\cloud_32x32.png icon.png
-
-        # Get the modules needed for actually running the code.
-        $productionModules = (npm ls --prod --parseable | Split-Path -Leaf) -ne $task
-
-        # Install build task to local tfs agent if it exists for rapid dev/test cycles.
-        if (Test-Path env:TfsBuildAgentPath) {
-            $agentTaskDir = [IO.Path]::Combine(
-                $env:TfsBuildAgentPath, "tasks", $task, "0.0.1")
-            if (Test-Path $agentTaskDir) {
-                Write-Verbose "Copying scripts for task $task to $agentTaskDir"
-                # Exclude non-packaged files.
-                $excludes = "node_modules", "obj", "bin", "Test", ".taskkey", "*.ts", "*.js.map", "package.json",
-                    "tsconfig.json", "manifest.json", "*.njsproj"
-                cp * $agentTaskDir -Force -Recurse -Exclude $excludes
-
-                $agentTaskModulesDir = Join-Path $agentTaskDir node_modules
-                $productionModules | %{
-                    $sourceDir = Join-Path node_modules $_
-                    Write-Verbose "cp $sourceDir $agentTaskModulesDir -Recurse -Force"
-                    cp $sourceDir $agentTaskModulesDir -Recurse -Force
-                }
-            }
-        }
-
-        # Create manifest.json in node_modules, so only production modules are
-        # included in the final package.
-        @{"files" = $productionModules | %{ @{"path"=$_} } } | ConvertTo-Json |
-          Out-File (Join-Path node_modules manifest.json) -Encoding utf8
-    } finally {
-        popd
+    if(Test-Path bin) {
+        Write-Verbose "Removing bin"
+        rm bin -Force -Recurse -ErrorAction Stop
     }
-    Write-Host "End build task $task"
-}
 
-function BuildCommon() {
-    Write-Host "Building common modules"
-    pushd common
-    try {
-        if (!$SkipNpm) {
-            Write-Verbose "Running: npm install"
-            npm install
-        }
-
-        Write-Verbose "Running: tsc"
-        tsc
-        if ($LASTEXITCODE -ne 0) {
-            throw "tsc failed for common modules"
-        }
-    } finally {
-        popd
+    if (-not $SkipInit){
+        InitAll $tasks
     }
-    Write-Host "End build common modules"
-}
 
-function InitPsTask($task) {
-    pushd $task
-    try {
-        if (-not (Test-Path ps_modules)) {
-            Write-Host "Start install VstsTaskSdk for $task"
-            mkdir ps_modules
-            cd ps_modules
-            Save-Module -Name VstsTaskSdk -Path .
-            $versionFolder = ls VstsTaskSdk
-            $items = ls $versionFolder.FullName -Force
-            Move-Item $items.FullName VstsTaskSdk
-            rm $versionFolder.FullName
-            Write-Host "End install VstsTaskSdk for $task"
-        }
-    } finally {
-        popd
+    if (-not $SkipCompile) {
+        CompileAll $tasks
     }
-}
+        
+    if (-not $SkipTest) {
+        TestAll $tasks
+    }
 
-Run $MyInvocation.MyCommand.Path
+    if (Test-Path env:TfsBuildAgentPath) {
+        PublishTasksLocal $tasks
+    }
+
+    if(-not $SkipPackage) {
+        Package $tasks
+    }
+} finally {
+    popd
+    $functionsModule | Remove-Module
+}
---
+++ build/BuildFunctions.psm1
@@ -0,0 +1,265 @@
+<##
+ # Copyright 2017 Google Inc.
+ # 
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ # 
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ # 
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ ##>
+
+function InitAll([string[]]$tasks) {
+    Write-Host "Initialize modules and tasks"
+    InitCommon
+    InitPsTask "install-cloud-sdk-build-task"
+    $tasks | % {
+        InitTsTask $_
+    }
+}
+
+function InitCommon(){
+    Write-Host "Initialize common modules"
+    pushd common
+    try {
+        if (-not (Test-Path node_modules)) {
+            npm install | Out-Null
+        }
+    } finally {
+        popd
+    }
+}
+
+function InitPsTask([string]$task) {
+    Write-Host "Initialize PowerShell task $task"
+    pushd $task
+    try {
+        if (-not (Test-Path ps_modules)) {
+            Write-Host "Installing VstsTaskSdk for $task"
+            mkdir ps_modules -ErrorAction Stop | cd
+            Save-Module -Name VstsTaskSdk -Path .
+            $versionFolder = ls VstsTaskSdk | Move-Item -Destination . -PassThru -Force
+            rm VstsTaskSdk
+            $finalFolder = $versionFolder | Rename-Item -NewName VstsTaskSdk -PassThru -Force
+            Write-Verbose "Wrote VstsTaskSdk to $finalFolder"
+        }
+    } finally {
+        popd
+    }
+}
+
+function InitTsTask([string]$task) {
+    Write-Host "Initialize TypeScript task $task"
+    pushd $task
+    try {
+        if (-not (Test-Path node_modules)) {
+            npm install | Out-Null
+        }
+    } finally {
+        popd
+    }
+}
+
+# Compile functions should be able to be replaced by MSBuild.
+function CompileAll([string[]]$tasks) {
+    Write-Host "Compiling TypeScript modules and tasks"
+    CompileCommon
+    $jobs = $tasks | % {
+        Start-Job -ArgumentList $pwd, $_ -ScriptBlock {
+            cd $args[0]
+            Import-Module ./build/BuildFunctions.psm1
+            CompileTask $args[1]
+        }
+    }
+    $jobs | Wait-Job | Receive-Job -Wait -AutoRemoveJob
+}
+
+function CompileCommon() {
+    Write-Host "Compiling common modules"
+    pushd common
+    try {
+        Write-Verbose "Running: tsc"
+        tsc
+        if ($LASTEXITCODE -ne 0) {
+            throw "tsc failed for common modules"
+        }
+    } finally {
+        popd
+    }
+}
+
+function CompileTask($task) {
+    Write-Host "Compiling task $task"
+    pushd $task
+    try {
+        # Common module changes as part of development.
+        # Update the common module every time.
+        npm install ../common | Out-Null
+
+        Write-Verbose "Running: tsc"
+        tsc
+        if ($LASTEXITCODE -ne 0) {
+            throw "tsc failed for task $task"
+        }
+    } finally {
+        popd
+    }
+}
+
+function TestAll([string[]]$tasks) {
+    Write-Host "Testing Tasks"
+    $jobs = $tasks | % {
+        Start-Job -ArgumentList $pwd, $_ -ScriptBlock {
+            cd $args[0]
+            Import-Module ./build/BuildFunctions.psm1
+            TestTask $args[1]
+        }
+    }
+    $jobs | Wait-Job | Receive-Job -Wait -AutoRemoveJob
+}
+
+function TestTask($task) {
+    Write-Host "Testing TypeScript task $task"
+    pushd $task
+    try {
+        $testSuite = Join-Path Test _suite.js
+
+        if (Test-path $testSuite) {
+            Write-Verbose "Running: mocha $testSuite"
+            mocha $testSuite
+            if ($LASTEXITCODE -ne 0) {
+                throw "mocha failed  for task $task"
+            }
+        } else {
+            Write-Warning "Skipping tests for task $task"
+        }
+    } finally {
+        popd
+    }
+}
+
+function RunAppveyorTests([string[]]$tasks) {
+    $jobs = $tasks | % {
+        Start-Job -ArgumentList $pwd, $_ -ScriptBlock {
+            $task = $args[1]
+            cd $args[0]
+            cd $task
+            vstest.console /logger:Appveyor  /UseVsixExtensions:true "$task.njsproj"
+        }
+    }
+    $jobs | Wait-Job | Receive-Job -Wait -AutoRemoveJob
+}
+
+function PublishTasksLocal([string[]]$tasks) {
+    $jobs = $tasks | % {
+        Start-Job -ArgumentList $pwd, $_ -ScriptBlock {
+            cd $args[0]
+            Import-Module ./build/BuildFunctions.psm1
+            PublishTsTaskLocal $args[1]
+        }
+    }
+    PublishPsTaskLocal "install-cloud-sdk-build-task"
+    $jobs | Wait-Job | Receive-Job -Wait -AutoRemoveJob
+}
+
+function PublishPsTaskLocal($task) {
+    Write-Host "Publishing PowerShell task $task to local build agent"
+    pushd $task
+    try {
+        $version = GetTaskVersion
+        # Install build task to local tfs agent for rapid dev/test cycles.
+        $agentTaskDir = [IO.Path]::Combine($env:TfsBuildAgentPath, "tasks", $task, $version)
+        if (Test-Path $agentTaskDir) {
+            Write-Verbose "Copying scripts for task $task to $agentTaskDir"
+            # Excluded non-packaged files.
+            $excludes = "obj", "bin", "Test", ".taskkey", "*.psproj"
+            cp * $agentTaskDir -Force -Recurse -Exclude $excludes
+        }
+    } finally {
+        popd
+    }
+}
+
+function PublishTsTaskLocal($task){
+    Write-Host "Publishing TypeScript task $task to local build agent"
+    pushd $task
+    try {
+        $version = GetTaskVersion
+        # Install build task to local tfs agent for rapid dev/test cycles.
+        $agentTaskDir = [IO.Path]::Combine(
+            $env:TfsBuildAgentPath, "tasks", $task, $version)
+        if (Test-Path $agentTaskDir) {
+            Write-Verbose "Copying scripts for task $task to $agentTaskDir"
+            # Excluded non-packaged files.
+            $excludes = "node_modules", "obj", "bin", "Test", ".taskkey", "*.ts", "*.js.map", "package.json",
+                "tsconfig.json", "manifest.json", "*.njsproj"
+            cp * $agentTaskDir -Force -Recurse -Exclude $excludes
+
+            $productionModules = GetProductionModules $task
+            $agentTaskModulesDir = Join-Path $agentTaskDir node_modules
+            $productionModules | % {
+                $sourceDir = Join-Path node_modules $_
+                Write-Verbose "cp $sourceDir $agentTaskModulesDir -Recurse -Force"
+                cp $sourceDir $agentTaskModulesDir -Recurse -Force
+            }
+        }
+    } finally {
+        popd
+    }
+}
+
+function Package([string[]]$tasks){
+
+    Write-Host "Building package"
+    $jobs = $tasks | % {
+        Start-Job -ArgumentList $pwd, $_ -ScriptBlock {
+            cd $args[0]
+            Import-Module ./build/BuildFunctions.psm1
+            PrePackageTask $args[1]
+        }
+    }
+    $jobs | Wait-Job | Receive-Job -Wait -AutoRemoveJob
+    Write-Verbose "Running: tfx extension create --output-path bin"
+    tfx extension create --manifestGlobs **/manifest.json --output-path bin
+}
+
+function PrePackageTask([string]$task) {
+    Write-Verbose "Running PrePackage tasks for $task"
+    pushd $task
+    try {
+        cp ..\images\cloud_32x32.png icon.png
+
+        # Get the modules needed for actually running the code.
+        $productionModules = GetProductionModules $task
+
+        # Create manifest.json in node_modules, so only production modules are
+        # included in the final package.
+        @{"files" = $productionModules | %{ @{ "path" = $_ } } } | ConvertTo-Json |
+            Out-File (Join-Path node_modules manifest.json) -Encoding utf8
+    } finally {
+        popd
+    }
+}
+
+function GetTaskVersion() {
+    $taskConfig = Get-Content task.json | ConvertFrom-Json
+    $parts = $taskConfig.version.PSObject.Properties | Sort Name
+    return $parts.Value -join "."
+}
+
+function GetProductionModules($task) {
+    return (npm ls --prod --parseable | Split-Path -Leaf) -ne $task
+}
+
+function GetTypeScriptTaskModules() {
+    $dirs = ls -Directory | ? {
+        $fileNames = ls $_ -File | Split-Path -Leaf
+        $fileNames -contains "task.json" -and $fileNames -contains "tsconfig.json"
+    }
+    $dirs.Name | Write-Output
+}

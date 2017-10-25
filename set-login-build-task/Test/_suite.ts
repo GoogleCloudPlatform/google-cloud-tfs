41227d374ec372f2142ebb7c256489798bb57a99
Jim Przybylinski <JimWP@google.com>
2017-10-25 16:49:25 -0700

    Revert "Add a failing test to test build failure."
    This reverts commit 80ff10c198f84959b190cf89f90504475db0206a.

--- set-login-build-task/Test/_suite.ts
+++ set-login-build-task/Test/_suite.ts
@@ -1,4 +1,4 @@
-// Copyright 2017 Google Inc. All Rights Reserved.
+// Copyright 2017 Google Inc. All Rights Reserved
 //
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
@@ -17,30 +17,25 @@
  *   outputs for set-login-build-task.
  * @author przybjw@google.com (Jim Przybylinski)
  */
-import * as assert from 'assert';
-import {
-  assertGcloudNotRun,
-  assertGcloudSilentSuccess,
-  assertKeyFileWritten
-} from 'common/asserts';
+import { MockTestRunner } from 'vsts-task-lib/mock-test';
 import * as path from 'path';
-import {MockTestRunner} from 'vsts-task-lib/mock-test';
+import * as assert from 'assert';
+import { assertGcloudSilentSuccess, assertGcloudNotRun, assertKeyFileWritten } from 'common/asserts'
 
 describe('set-login-build-task tests', () => {
 
   let runner: MockTestRunner;
-  beforeEach(() => { runner = null; });
+  beforeEach(function () {
+    runner = null;
+  });
 
-  afterEach(function(): void {
+  afterEach(function () {
     if (this.currentTest.state === 'failed') {
       console.log(runner.stdout);
       console.log('--------------------');
       console.log(runner.stderr);
     }
   });
-
-  it('will fail to test build',
-     () => { assert.ok(false, 'Testing build failure'); });
 
   it('should fail for no gcloud', () => {
     const testPath = path.join(__dirname, 'no-gcloud.js');
@@ -59,17 +54,18 @@ describe('set-login-build-task tests', () => {
     assert.equal(runner.invokedToolCount, 1, 'Should have run gcloud once.');
     assert.equal(runner.warningIssues.length, 0, 'Should have no warnings.');
     assert.equal(runner.errorIssues.length, 1, 'Should have an error.');
-    assert(runner.createdErrorIssue('[gcloud error]'),
-           'Should write stderr as issue.');
+    assert(
+        runner.createdErrorIssue('[gcloud error]'),
+        'Should write stderr as issue.');
     assert(!runner.stdOutContained('[gcloud output]'),
-           'gcloud output should not be visible.');
+      'gcloud output should not be visible.');
     assertKeyFileWritten(runner);
     assert(!runner.stdOutContained('[task.setvariable variable=ipVar'),
-           'Should not have set ip variable.');
+      'Should not have set ip variable.');
     assert(!runner.stdOutContained('[task.setvariable variable=passwordVar'),
-           'Should not have set password variable.');
+      'Should not have set password variable.');
     assert(!runner.stdOutContained('[task.setvariable variable=userNameVar'),
-           'Should not have set user name variable.');
+      'Should not have set user name variable.');
   });
 
   it('should work with given user name inputs', () => {
@@ -79,11 +75,11 @@ describe('set-login-build-task tests', () => {
 
     assertGcloudSilentSuccess(runner);
     assert(runner.stdOutContained('[task.setvariable variable=ipVar'),
-           'Should have set ip variable.');
+      'Should have set ip variable.');
     assert(runner.stdOutContained('[task.setvariable variable=passwordVar'),
-           'Should have set password variable.');
+      'Should have set password variable.');
     assert(!runner.stdOutContained('[task.setvariable variable=userNameVar'),
-           'Should not have set user name variable.');
+      'Should not have set user name variable.');
   });
 
   it('should fail with missing endpoint parameter', () => {
@@ -93,7 +89,7 @@ describe('set-login-build-task tests', () => {
 
     assertGcloudNotRun(runner);
     assert(!runner.stdOutContained('[task.setvariable'),
-           'Should not have set any variables.');
+      'Should not have set any variables.');
   });
 
   it('should fail with missing zone parameter', () => {
@@ -103,7 +99,7 @@ describe('set-login-build-task tests', () => {
 
     assertGcloudNotRun(runner);
     assert(!runner.stdOutContained('[task.setvariable'),
-           'Should not have set any variables.');
+      'Should not have set any variables.');
   });
 
   it('should fail with missing machine parameter', () => {
@@ -113,20 +109,20 @@ describe('set-login-build-task tests', () => {
 
     assertGcloudNotRun(runner);
     assert(!runner.stdOutContained('[task.setvariable'),
-           'Should not have set any variables.');
+      'Should not have set any variables.');
   });
 
   it('should fail with missing user name parameter with given ' +
-         'userNameType.',
-     () => {
-       const testPath = path.join(__dirname, 'missing-user-name.js');
-       runner = new MockTestRunner(testPath);
-       runner.run();
+      'userNameType.',
+      () => {
+        const testPath = path.join(__dirname, 'missing-user-name.js');
+        runner = new MockTestRunner(testPath);
+        runner.run();
 
-       assertGcloudNotRun(runner);
-       assert(!runner.stdOutContained('[task.setvariable'),
-              'Should not have set any variables.');
-     });
+        assertGcloudNotRun(runner);
+        assert(!runner.stdOutContained('[task.setvariable'),
+          'Should not have set any variables.');
+      });
 
   it('should fail with missing machineIpOutput parameter', () => {
     const testPath = path.join(__dirname, 'missing-machine-ip-output.js');
@@ -135,7 +131,7 @@ describe('set-login-build-task tests', () => {
 
     assertGcloudNotRun(runner);
     assert(!runner.stdOutContained('[task.setvariable'),
-           'Should not have set any variables.');
+      'Should not have set any variables.');
   });
 
   it('should fail with missing passwordOutput parameter', () => {
@@ -145,31 +141,29 @@ describe('set-login-build-task tests', () => {
 
     assertGcloudNotRun(runner);
     assert(!runner.stdOutContained('[task.setvariable'),
-           'Should not have set any variables.');
+      'Should not have set any variables.');
   });
 
   it('should fail with missing password from gcloud output', () => {
     const testPath = path.join(__dirname, 'gcloud-output-missing-password.js');
     runner = new MockTestRunner(testPath);
     runner.run();
-
+    
     assert(runner.failed, 'Should have failed.');
     assert.equal(runner.invokedToolCount, 1, 'Should have run gcloud once.');
     assert.equal(runner.warningIssues.length, 0, 'Should have no warnings.');
     assert.equal(runner.errorIssues.length, 1, 'Should have an error.');
     assertKeyFileWritten(runner);
 
-    assert(runner.createdErrorIssue(
-               'Could not find new password for instance instanceId' +
-               ' in zone zoneId'),
-           'Should not have set ip variable.');
+    assert(runner.createdErrorIssue('Could not find new password for instance instanceId' +
+      ' in zone zoneId'),
+      'Should not have set ip variable.');
     assert(!runner.stdOutContained('[task.setvariable variable=passwordVar'),
-           'Should not have set password variable.');
+      'Should not have set password variable.');
   });
 
   it('should fail with missing ip_address from gcloud output', () => {
-    const testPath =
-        path.join(__dirname, 'gcloud-output-missing-ip_address.js');
+    const testPath = path.join(__dirname, 'gcloud-output-missing-ip_address.js');
     runner = new MockTestRunner(testPath);
     runner.run();
 
@@ -179,11 +173,10 @@ describe('set-login-build-task tests', () => {
     assert.equal(runner.errorIssues.length, 1, 'Should have an error.');
     assertKeyFileWritten(runner);
 
-    assert(runner.createdErrorIssue(
-               'Could not find external ip for instance instanceId' +
-               ' in zone zoneId'),
-           'Should not have set ip variable.');
+    assert(runner.createdErrorIssue('Could not find external ip for instance instanceId' +
+      ' in zone zoneId'),
+      'Should not have set ip variable.');
     assert(!runner.stdOutContained('[task.setvariable variable=ipVar'),
-           'Should not have set ip variable.');
+      'Should not have set ip variable.');
   });
 });

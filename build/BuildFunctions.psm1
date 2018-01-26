1ea5889bf9e917cf748110aa82993c3699db1dcf
Jim Przybylinski <JimWP@google.com>
2018-01-26 12:48:21 -0800

    Use String interpolation rather than concatination.

--- build/BuildFunctions.psm1
+++ build/BuildFunctions.psm1
@@ -150,7 +150,7 @@ function Invoke-MochaTest([string]$task, [string]$reporter) {
 
 function Send-Coverage() {
     Write-Host "Sending Code Coverage reports."
-    $env:PATH = 'C:\msys64\usr\bin;' + $env:PATH
+    $env:PATH = "C:\msys64\usr\bin;$env:PATH"
     Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile codecov.sh
     bash codecov.sh
 }
